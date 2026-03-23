c63433435e8e6fffde5e484916420a9121472914
KhomotsoMashupye <motsomash2242@gmail.com>
2026-03-23 09:53:49 +0200

    set up the eks cluster and added a managed node group

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,3 +102,79 @@ resource "aws_route_table_association" "eks_private_assoc" {
   subnet_id      = aws_subnet.eks_private_subnet[count.index].id
   route_table_id = aws_route_table.eks_private_rt[count.index].id
 }
+# EKS Cluster Setup
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role-usw2"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "eks.amazonaws.com" } }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks-banking-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids              = aws_subnet.eks_private_subnet[*].id
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_cloudwatch_log_group.eks_cluster_logs
+  ]
+}
+
+
+# Managed Node Group
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role-usw2"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" # Policy path fix
+  ])
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = each.value
+}
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "banking-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.eks_private_subnet[*].id
+  ami_type        = "AL2023_x86_64_STANDARD" 
+  instance_types  = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  timeouts {
+    delete = "20m"
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.node_policies]
+}
